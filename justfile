0a04de7225427e1adc44f524fda96e307809f9c9
David Balatero <dbalatero@users.noreply.github.com>
2026-07-18 16:26:31 -0400

    chore: set up pytest

--- justfile
+++ justfile
@@ -1,4 +1,8 @@
 format:
     uv run ruff format .
+
 lint:
     uv run ruff check .
+
+test:
+    uv run pytest
---
+++ tests/test_hello.py
@@ -0,0 +1,5 @@
+from keyvalue import hello
+
+
+def test_hello() -> None:
+    assert hello() == "Hello from keyvalue-py!"
