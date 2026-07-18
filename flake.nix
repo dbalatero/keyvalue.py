3ec52742127dbf0b63b9890ad68eaec2397b757b
David Balatero <dbalatero@users.noreply.github.com>
2026-07-18 16:22:31 -0400

    chore: initial tooling via Nix/direnv

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Python key/value server development shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      supportedSystems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              just
+              netcat
+              python312
+              ruff
+              uv
+            ];
+
+            env = {
+              UV_PROJECT_ENVIRONMENT = ".venv";
+            };
+          };
+        }
+      );
+    };
+}
