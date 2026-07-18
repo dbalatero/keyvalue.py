9c1fcab0bd226af08b0d308e2fcda2e31704b439
David Balatero <dbalatero@users.noreply.github.com>
2026-07-18 18:28:17 -0400

    fix: drop Darwin support from flake.nix

--- flake.nix
+++ flake.nix
@@ -5,39 +5,32 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs =
-    { nixpkgs, ... }:
-    let
-      supportedSystems = [
-        "aarch64-darwin"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "x86_64-linux"
-      ];
+  outputs = {nixpkgs, ...}: let
+    supportedSystems = [
+      "aarch64-linux"
+      "x86_64-linux"
+    ];
 
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-    in
-    {
-      devShells = forAllSystems (
-        system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-        in
-        {
-          default = pkgs.mkShell {
-            packages = with pkgs; [
-              just
-              netcat
-              python312
-              ruff
-              uv
-            ];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+  in {
+    devShells = forAllSystems (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            just
+            netcat
+            python312
+            ruff
+            uv
+          ];
 
-            env = {
-              UV_PROJECT_ENVIRONMENT = ".venv";
-            };
+          env = {
+            UV_PROJECT_ENVIRONMENT = ".venv";
           };
-        }
-      );
-    };
+        };
+      }
+    );
+  };
 }
